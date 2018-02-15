b63173f2a14f1c2883383b277c22f24ad8ad1fe9
CameronMurr <32745910+cameronmurr@users.noreply.github.com>
2018-02-15 13:48:58 -0500

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,31 @@
+def shift(letter, shift_amount):
+    unicode_value=ord(letter) + shift_amount
+
+    new_letter=chr(unicode_value)
+
+    return new_letter
+
+def encrypt(message, shift_amount):
+    result=""
+
+    for letter in message:
+        result+=shift(letter, shift_amount)
+
+    return result
+
+
+def decrypt(message, shift_amount):
+    result=""
+
+    for letter in message:
+        result+=shift(letter, -shift_amount)
+    return result
+
+secret_message = "I really hope this works!"
+encrypted_message = encrypt(secret_message, 3)
+decrypted_message=decrypt(encrypted_message, 3)
+
+print(secret_message)
+print(encrypted_message)
+print(decrypted_message)
+
